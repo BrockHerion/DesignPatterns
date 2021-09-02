b4b4c6d624ddd61d1a06bcfa5f7ae4c490d0a25a
Brock Herion <brockheriondev@gmail.com>
2021-09-01 20:02:07 -0500

    Updated project to .NET 6

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
+using StrategyPattern;
 
-namespace StrategyPattern
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var mallard = new MallardDuck();
-            mallard.PerformQuack();
-            mallard.PerformFly();
 
-            var model = new ModelDuck();
-            model.PerformFly();
-            model.FlyBehavior = new FlyPoweredRocket();
-            model.PerformFly();
-        }
-    }
-}
+var mallard = new MallardDuck();
+mallard.PerformQuack();
+mallard.PerformFly();
+
+var model = new ModelDuck();
+model.PerformFly();
+model.FlyBehavior = new FlyPoweredRocket();
+model.PerformFly();
